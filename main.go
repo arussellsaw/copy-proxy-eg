adeff85915ad0b881527448e6ad0ca2f07968724
arussellsaw <alexrussellsaw@gmail.com>
2015-05-28 00:37:41 +0100

    use httputil

--- main.go
+++ main.go
@@ -1,46 +1,23 @@
-package main;
+package main
 
 import (
-
-	"net/http"
 	"log"
-	"io"
+	"net/http"
+	"net/http/httputil"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request){
-
-		//create the new request
-		outreq := new(http.Request)
-		*outreq = *req // includes shallow copies of maps, but okay
-		outreq.URL.Scheme = "http"
-		outreq.URL.Host = "localhost:8082" //new address
-
-		//proxy transport
-		transport := http.DefaultTransport
-		transport.RoundTrip(outreq)
-
-		//resend the request
-		res, err := transport.RoundTrip(outreq)
-		if err != nil {
-			log.Print("http: proxy error: ", err)
-			resp.WriteHeader(http.StatusInternalServerError)
-			return
+	http.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
+		proxy := &httputil.ReverseProxy{
+			Director: func(req *http.Request) {
+				req.URL.Scheme = "http"
+				req.URL.Host = "localhost:8082"
+			},
 		}
-		defer res.Body.Close()
-
-		//copy the headers
-		for k, vv := range res.Header {
-			for _, v := range vv {
-				resp.Header().Add(k, v)
-			}
-		}
-
-		//copy the body
-		io.Copy(resp, res.Body)
-	});
+		proxy.ServeHTTP(resp, req)
+	})
 
 	log.Print("started...")
-	log.Fatal(http.ListenAndServe(":8081", nil));
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
